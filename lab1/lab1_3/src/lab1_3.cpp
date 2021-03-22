fd90da98e112f9ef76ef56792da9237bef898db5
dinogrgic1 <dino.grgic1@gmail.com>
2021-03-22 20:57:49 +0100

    Finished lab1_3

--- lab1/lab1_3/src/lab1_3.cpp
+++ lab1/lab1_3/src/lab1_3.cpp
@@ -5,7 +5,6 @@ class CoolClass
 public:
     virtual void set(int x) { x_ = x; };
     virtual int get() { return x_; };
-
 private:
     int x_;
 };
@@ -20,8 +19,8 @@ private:
     int x_;
 };
 
-int main(int argc, char *argv[])
+int main()
 {
-    std::cout << "CoolClass size: " << sizeof(CoolClass) << std::endl;
-    std::cout << "PlainOldClass size: " << sizeof(PlainOldClass) << std::endl;
+    std::cout << "CoolClass size: " << sizeof(CoolClass) << " bytes" << std::endl;
+    std::cout << "PlainOldClass size: " << sizeof(PlainOldClass) << " bytes" << std::endl;
 }
